33020651e7339a37f8d038df1a47ac0973e2a42e
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-06-07 12:55:22 +0600

    Footer Added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,17 +1,70 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Grid, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import logo from "../../../assets/Camp Craftopia.png"
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <div>
-             <Box bg="gray.200" py={4} textAlign="center">
-             <Link href="/" fontSize="xl" fontWeight="bold">
-            <img src={logo} width={80} alt="" />
-          </Link>
-    </Box>
-        </div>
-    );
+  return (
+    <>
+      <hr style={{ borderColor: "#FFD9EC", opacity: 1, borderWidth: "2px" }} />
+      <Box bg="#FFFFFF" color="#000" py={4}>
+        <Grid
+          templateColumns="1fr 1fr 1fr"
+          gap={6}
+          alignItems="start"
+          justifyItems="center"
+          px={20}
+        >
+          <Box>
+            <Text fontSize="3xl" fontWeight="bold" lineHeight="1.5">
+              Camp Craftopia
+            </Text>
+            <Text fontSize="lg">Address: Dhaka, Bangladesh</Text>
+
+            <Flex fontSize="3xl" justifyContent="center" mt={4} ml={-5}>
+              <Link
+                href="https://www.facebook.com/"
+                target="_blank"
+                style={{ marginRight: "8px" }}
+              >
+                <FaFacebook />
+              </Link>
+              <Link
+                href="https://www.instagram.com/"
+                target="_blank"
+                style={{ marginRight: "8px" }}
+              >
+                <FaInstagram />
+              </Link>
+              <Link href="https://linkedin.com/" target="_blank">
+                <FaLinkedin />
+              </Link>
+            </Flex>
+          </Box>
+          <Box style={{ lineHeight: "28px" }}>
+            <Text fontSize="lg" fontWeight="bold" marginBottom="6px">
+              Company
+            </Text>
+            <p>About Us</p>
+            <p>Terms of Use</p>
+            <p>Privacy Policy</p>
+          </Box>
+          <Box style={{ lineHeight: "28px" }}>
+          <Text fontSize="lg" fontWeight="bold" marginBottom="6px">
+              Support
+            </Text>
+            <p>FAQ</p>
+            <p>Quick Chat</p>
+            <p>Business</p>
+          </Box>
+        </Grid>
+      </Box>
+      <Box align="center" mt={8}>
+        <Text>
+          &copy; {new Date().getFullYear()} Camp Craftopia
+        </Text>
+      </Box>
+    </>
+  );
 };
 
 export default Footer;
